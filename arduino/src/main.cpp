4950e49535e8b94163e71d3b1f2710d3a02697c3
Juri Berlanda <juriberlanda@hotmail.com>
2023-11-04 21:09:14 +0100

    [Arduino] Add OLED.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,18 +1,73 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
 #include <SimpleDHT.h>
 
-// for DHT22,
-//      VCC: 5V or 3V
-//      GND: GND
-//      DATA: 2
-
-int pinDHT22 = 17;
 int pinLED = 25;
+
+// DHT22
+int pinDHT22 = 17;
 SimpleDHT22 dht22(pinDHT22);
+
+// OLED
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(128, 32, new MbedI2C(14, 15), OLED_RESET);
+
+void drawGradeSymbol() {
+  uint16_t gap = 4;
+  uint16_t radius = 3;
+  uint16_t x = display.getCursorX() + gap;
+  uint16_t y = display.getCursorY();
+
+  display.drawCircle(x, y + radius, radius, SSD1306_WHITE);
+  display.drawCircle(x, y + radius, radius - 1, SSD1306_WHITE);
+
+  display.setCursor(x + 3 + gap, y);
+}
+
+void printTempAndHumidity(float temperature, float humidity) {
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+
+  display.print(F("H: "));
+  display.print(humidity);
+  display.println(F("%"));
+
+  display.print(F("T: "));
+  display.print(temperature);
+  drawGradeSymbol();
+  display.println(F("C"));
+
+  display.display();
+  delay(2000);
+}
 
 void setup() {
   Serial.begin(9600);
   pinMode(pinLED, OUTPUT);
   digitalWrite(pinLED, HIGH);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;) {
+      digitalWrite(pinLED, LOW);
+      delay(500);
+      digitalWrite(pinLED, HIGH);
+      delay(500);
+    } // Don't proceed, loop forever
+  }
+  display.dim(true);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 8);
+  display.println("Reading...");
+  display.display();
 }
 
 void loop() {
@@ -37,6 +92,7 @@ void loop() {
   Serial.println(humidity);
 
   digitalWrite(LED_BUILTIN, humidity > 60);
+  printTempAndHumidity(temperature, humidity);
 
   // DHT22 sampling rate is 0.5HZ.
   for (int i = 0; i < 2; i++)
